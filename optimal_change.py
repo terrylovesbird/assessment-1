865d16fd9a90dead8c01e4ccb2a6d6f72105e510
terrylovesbird <tianlin.cai@gmail.com>
2021-06-04 15:29:39 -0500

    Tianlin's assessment-1 solution & unittest

--- optimal_change.py
+++ optimal_change.py
@@ -1,1 +1,55 @@
 # Write your solution here!
+def optimal_change(item_cost, amount_paid):
+    #calculate how much is the change
+    change = amount_paid - item_cost
+    #having a dict of denominations, bill : value; Since its "optimal", so the dict's order is from biggest bill to smallest bill
+    denos = {
+        '$100 bill': 100,
+        '$50 bill': 50,
+        '$20 bill': 20,
+        '$10 bill': 10,
+        '$5 bill': 5,
+        '$1 bill': 1,
+        'quarter': 0.25,
+        'dime': 0.10,
+        'nickel': 0.05,
+        'penny': 0.01
+    }
+
+    #initialize the answer
+    answer = f"The optimal change for an item that costs ${item_cost} with an amount paid of ${amount_paid} is "
+
+    #some edge or special cases, like you dont pay or pay is less than the cost, also when exact pay w/o any change needed
+    if change < 0:
+        answer += 'Please pay with enough amount!'
+    elif change == 0:
+        answer += 'No changes needed!'
+    
+    #when change > 0, compare the change through the denos dict
+    elif change > 0:
+        for deno_string in denos:
+            #how many times to pay for each deno_string
+            times = int(change//denos[deno_string])
+            # if deno_string is penny and plural
+            if times > 1 and deno_string == 'penny': 
+                answer += f"and {times} pennies."
+            # if deno_string is penny and singular
+            elif times == 1 and deno_string == 'penny': 
+                answer += f"and 1 penny."
+            # if deno_string is else and plural
+            elif times > 1:
+                answer += f"{times} {deno_string}s, "
+            # if deno_string is else and singular
+            elif times ==1:
+                answer += f"1 {deno_string}, "
+            
+            #update change
+            change -= times * denos[deno_string] 
+            #in debug mode, found some defect could happen to make change like 0.019999, so round here to get correct times
+            change = round(change, 2)
+        #when change happens, replace the last punctuation with "."
+        if answer[-2] == ",": # if the 2nd last punctuation is ","
+            answer = answer[:-2]+"." # replace the last 2 chars with a "."
+        
+    return answer
+#print(optimal_change(30, 100)) 
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,1 +1,30 @@
 # Write your unit tests here!
+import unittest
+from optimal_change import optimal_change
+
+class TestOptimalChange(unittest.TestCase):
+    #test edge and special cases
+    def test_exact_pay(self):
+        self.assertTrue(optimal_change(51.53, 51.53) == "The optimal change for an item that costs $51.53 with an amount paid of $51.53 is No changes needed!")
+
+    def test_no_enough_pay(self):
+        self.assertTrue(optimal_change(9.87, 0) == "The optimal change for an item that costs $9.87 with an amount paid of $0 is Please pay with enough amount!")
+
+    def test_single_domination_1(self):
+        self.assertEqual(optimal_change(50, 100), "The optimal change for an item that costs $50 with an amount paid of $100 is 1 $50 bill.")
+
+    def test_multi_domination_1(self):
+        self.assertEqual(optimal_change(3, 50), "The optimal change for an item that costs $3 with an amount paid of $50 is 2 $20 bills, 1 $5 bill, 2 $1 bills.")
+
+    #some normal cases with penny
+    def test_normal_cases_1(self):
+        self.assertEqual(optimal_change(62.13, 100), "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
+
+    def test_normal_cases_2(self):
+        self.assertEqual(optimal_change(31.51, 50), "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
+
+    def test_normal_cases_3(self):
+        self.assertEqual(optimal_change(0.01, 99), "The optimal change for an item that costs $0.01 with an amount paid of $99 is 1 $50 bill, 2 $20 bills, 1 $5 bill, 3 $1 bills, 3 quarters, 2 dimes, and 4 pennies.") 
+              
+if __name__ == '__main__':
+    unittest.main()
